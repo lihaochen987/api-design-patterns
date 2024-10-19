ad73896e1712b2bb230adc4395f5a15946b2d1d0
HaoChen <lihaochen987@gmail.com>
2024-10-20 10:17:42 +1300

    Add Sqlite database

---
+++ backend/backend/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Database;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Laptop", Price = 1000m },
+            new Product { Id = 2, Name = "Smartphone", Price = 500m }
+        );
+    }
+}
