fbb4f708ea8ed49e2de836aad09ea19e67f6742c
HaoChen <lihaochen987@gmail.com>
2024-10-20 15:34:28 +1300

    Fix delete standard method

--- backend/backend/Controllers/ProductController.cs
+++ backend/backend/Controllers/ProductController.cs
@@ -88,7 +88,8 @@ public class ProductController(ApplicationDbContext context) : ControllerBase
     [HttpDelete]
     public async Task<ActionResult> DeleteProduct([FromQuery] DeleteProductRequest request)
     {
-        var product = await context.Products.FindAsync(request.Id);
+        if (!long.TryParse(request.Id, out var id)) return BadRequest();
+        var product = await context.Products.FindAsync(id);
         if (product == null)
         {
             return NotFound();
