43bec992fd7fa75419d01e8c1c11ef1b146b9b64
HaoChen <lihaochen987@gmail.com>
2024-10-26 12:07:48 +1300

    Add db migrations

---
+++ backend/backend/Migrations/20241025230229_AddDimensionsToProduct.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDimensionsToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 9L);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 10L);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProductId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Height",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Length",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Width",
+                table: "Products",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ProductId",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "ProductCategory", "ProductName", "ProductPrice" },
+                values: new object[,]
+                {
+                    { 1L, "DogFood", "Dry Dog Food", 50m },
+                    { 2L, "DogFood", "Wet Dog Food", 35m },
+                    { 3L, "DogFood", "Dog Treats", 10m },
+                    { 4L, "Toys", "Chew Toy", 15m },
+                    { 5L, "Toys", "Fetch Ball", 8m },
+                    { 6L, "CollarsAndLeashes", "Dog Collar", 12m },
+                    { 7L, "CollarsAndLeashes", "Dog Leash", 20m },
+                    { 8L, "GroomingAndHygiene", "Dog Shampoo", 10m },
+                    { 9L, "GroomingAndHygiene", "Dog Brush", 7m },
+                    { 10L, "Beds", "Comfort Dog Bed", 80m }
+                });
+        }
+    }
+}
