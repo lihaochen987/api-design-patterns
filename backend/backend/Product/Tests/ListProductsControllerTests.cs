9c5a644b32d89eaa738a67d215e73abd232aa8c4
HaoChen <lihaochen987@gmail.com>
2024-10-27 08:50:06 +1300

    Add more pagination tests

--- backend/backend/Product/Tests/ListProductsControllerTests.cs
+++ backend/backend/Product/Tests/ListProductsControllerTests.cs
@@ -47,6 +47,27 @@ public class ListProductsControllerTests : IDisposable
         var listProductsResponse = response.Value as ListProductsResponse;
         listProductsResponse!.Results.Count().ShouldBe(4);
     }
+    
+    [Fact]
+    public async Task ListProducts_ShouldReturnProductsAfterPageToken_WhenPageTokenProvided()
+    {
+        _dbContext.Products.AddRange(_fixture.CreateMany<DomainModels.Product>(4));
+        await _dbContext.SaveChangesAsync();
+
+        var request = new ListProductsRequest { PageToken = "2", MaxPageSize = 2 };
+
+        var result = await _controller.ListProducts(request);
+        
+        result.Result.ShouldNotBeNull();
+        result.Result.ShouldBeOfType<OkObjectResult>();
+        var response = result.Result as OkObjectResult;
+        response.ShouldNotBeNull();
+        var listProductsResponse = response.Value as ListProductsResponse;
+        listProductsResponse!.Results.Count().ShouldBe(2);
+        listProductsResponse.Results.First()!.Id.ShouldBe(3);
+        listProductsResponse.Results.Last()!.Id.ShouldBe(4);
+        listProductsResponse.NextPageToken.ShouldBeNull();
+    }
 
     [Fact]
     public async Task ListProducts_ShouldReturnNextPageToken_WhenMoreProductsExist()
@@ -65,6 +86,22 @@ public class ListProductsControllerTests : IDisposable
         var listProductsResponse = response.Value as ListProductsResponse;
         listProductsResponse!.Results.Count().ShouldBe(2);
         listProductsResponse.NextPageToken.ShouldBeEquivalentTo("2");
+    }
+    
+    [Fact]
+    public async Task ListProducts_ShouldReturnEmptyList_WhenNoProductsExist()
+    {
+        var request = new ListProductsRequest { MaxPageSize = 2 };
+
+        var result = await _controller.ListProducts(request);
+
+        result.Result.ShouldNotBeNull();
+        result.Result.ShouldBeOfType<OkObjectResult>();
+        var response = result.Result as OkObjectResult;
+        response.ShouldNotBeNull();
+        var listProductsResponse = response.Value as ListProductsResponse;
+        listProductsResponse!.Results.ShouldBeEmpty();
+        listProductsResponse.NextPageToken.ShouldBeNull();
     }
 
     public void Dispose()
