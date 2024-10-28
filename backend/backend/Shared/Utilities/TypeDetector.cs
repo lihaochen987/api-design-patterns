4e2358a310f3f476dcf85228fd4a69276d3532a1
HaoChen <lihaochen987@gmail.com>
2024-10-28 15:42:11 +1300

    Remove unused class

--- backend/backend/Shared/Utilities/TypeDetector.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Shared.Utilities;
-
-public static class TypeDetector
-{
-    public static bool IsComplexType(Type type)
-    {
-        return !type.IsPrimitive && type != typeof(string) && !type.IsEnum;
-    }
-}
