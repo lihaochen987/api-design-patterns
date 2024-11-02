a98258149734802174e1e85e26e9a7a47bf4acd6
HaoChen <lihaochen987@gmail.com>
2024-11-03 12:03:50 +1300

    Fix all tests to use Postgresql

--- backend/backend/Product/Tests/CreateProductControllerTests.cs
+++ backend/backend/Product/Tests/CreateProductControllerTests.cs
@@ -1,69 +1,68 @@
-// using System.Globalization;
-// using AutoFixture;
-// using backend.Database;
-// using backend.Product.Contracts;
-// using backend.Product.Controllers;
-// using backend.Shared;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Shouldly;
-// using Xunit;
-// using Category = backend.Product.DomainModels.Category;
-//
-// namespace backend.Product.Tests;
-//
-// [Collection("SequentialExecutionCollection")]
-// public class CreateProductControllerTests : IDisposable
-// {
-//     private readonly Fixture _fixture = new();
-//     private readonly CreateProductController _controller;
-//     private readonly ApplicationDbContext _dbContext;
-//     private readonly CreateProductExtensions _extensions;
-//
-//     public CreateProductControllerTests()
-//     {
-//         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-//             .UseSqlite($"Filename=:memory:{Guid.NewGuid()};Mode=Memory;Cache=Shared")
-//             .Options;
-//
-//         var db = new ApplicationDbContext(options);
-//         db.Database.EnsureCreated();
-//         _dbContext = db;
-//         _extensions = new CreateProductExtensions(new TypeParser());
-//         _controller = new CreateProductController(db, _extensions);
-//     }
-//
-//     [Fact]
-//     public async Task CreateProduct_Should_AddProduct_And_ReturnCreatedAtActionResult()
-//     {
-//         var request = new CreateProductRequest
-//         {
-//             Name = _fixture.Create<string>(),
-//             Price = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//             Category = _fixture.Create<Category>().ToString(),
-//             Dimensions = new DimensionsContract
-//             {
-//                 Length = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//                 Width = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//                 Height = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture)
-//             }
-//         };
-//         var expectedProduct = _extensions.ToEntity(request);
-//         var expectedResponse = _extensions.ToCreateProductResponse(expectedProduct);
-//
-//         var result = await _controller.CreateProduct(request);
-//
-//         var createdAtActionResult = result.Result.ShouldBeOfType<CreatedAtActionResult>();
-//         createdAtActionResult.ActionName.ShouldBe("GetProduct");
-//         createdAtActionResult.ControllerName.ShouldBe("GetProduct");
-//         var actualResponse = createdAtActionResult.Value.ShouldBeOfType<CreateProductResponse>();
-//         actualResponse.ShouldBeEquivalentTo(expectedResponse);
-//     }
-//
-//     public void Dispose()
-//     {
-//         _dbContext.Database.EnsureDeleted();
-//         _dbContext.Dispose();
-//         GC.SuppressFinalize(this);
-//     }
-// }
+using System.Globalization;
+using AutoFixture;
+using backend.Database;
+using backend.Product.Contracts;
+using backend.Product.Controllers;
+using backend.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+using Category = backend.Product.DomainModels.Category;
+
+namespace backend.Product.Tests;
+
+[Collection("SequentialExecutionCollection")]
+public class CreateProductControllerTests : IDisposable
+{
+    private readonly Fixture _fixture = new();
+    private readonly CreateProductController _controller;
+    private readonly ApplicationDbContext _dbContext;
+    private readonly CreateProductExtensions _extensions;
+
+    public CreateProductControllerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql("Host=localhost;Database=mytestdatabase;Username=myusername;Password=mypassword")
+            .Options;
+
+        _dbContext = new ApplicationDbContext(options);
+        _dbContext.Database.EnsureCreated(); // Ensure the database schema is created
+        _extensions = new CreateProductExtensions(new TypeParser());
+        _controller = new CreateProductController(_dbContext, _extensions);
+    }
+
+    [Fact]
+    public async Task CreateProduct_Should_AddProduct_And_ReturnCreatedAtActionResult()
+    {
+        var request = new CreateProductRequest
+        {
+            Name = _fixture.Create<string>(),
+            Price = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+            Category = _fixture.Create<Category>().ToString(),
+            Dimensions = new DimensionsContract
+            {
+                Length = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                Width = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                Height = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture)
+            }
+        };
+        var expectedProduct = _extensions.ToEntity(request);
+        var expectedResponse = _extensions.ToCreateProductResponse(expectedProduct);
+
+        var result = await _controller.CreateProduct(request);
+
+        var createdAtActionResult = result.Result.ShouldBeOfType<CreatedAtActionResult>();
+        createdAtActionResult.ActionName.ShouldBe("GetProduct");
+        createdAtActionResult.ControllerName.ShouldBe("GetProduct");
+        var actualResponse = createdAtActionResult.Value.ShouldBeOfType<CreateProductResponse>();
+        actualResponse.ShouldBeEquivalentTo(expectedResponse);
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- backend/backend/Product/Tests/ReplaceProductControllerTests.cs
+++ backend/backend/Product/Tests/ReplaceProductControllerTests.cs
@@ -1,74 +1,74 @@
-// using System.Globalization;
-// using AutoFixture;
-// using backend.Database;
-// using backend.Product.Contracts;
-// using backend.Product.Controllers;
-// using backend.Product.DomainModels;
-// using backend.Shared;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Shouldly;
-// using Xunit;
-//
-// namespace backend.Product.Tests;
-//
-// [Collection("SequentialExecutionCollection")]
-// public class ReplaceProductControllerTests : IDisposable
-// {
-//     private readonly Fixture _fixture = new();
-//     private readonly ReplaceProductController _controller;
-//     private readonly ApplicationDbContext _dbContext;
-//     private readonly ReplaceProductExtensions _extensions;
-//
-//     public ReplaceProductControllerTests()
-//     {
-//         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-//             .UseSqlite($"Filename=:memory:{Guid.NewGuid()}")
-//             .Options;
-//
-//         var db = new ApplicationDbContext(options);
-//         db.Database.EnsureCreated();
-//         _dbContext = db;
-//         _extensions = new ReplaceProductExtensions(new TypeParser());
-//         _controller = new ReplaceProductController(_dbContext, _extensions);
-//     }
-//
-//     [Fact]
-//     public async Task ReplaceProduct_Should_ReturnOk_WithUpdatedProduct_When_ProductExists()
-//     {
-//         var originalProduct = new ProductTestDataBuilder().Build();
-//         _dbContext.Products.Add(originalProduct);
-//         await _dbContext.SaveChangesAsync();
-//
-//         var request = new ReplaceProductRequest
-//         {
-//             Name = _fixture.Create<string>(),
-//             Price = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//             Category = _fixture.Create<Category>().ToString(),
-//             Dimensions = new DimensionsContract
-//             {
-//                 Length = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//                 Width = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
-//                 Height = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture)
-//             }
-//         };
-//
-//         var result = await _controller.ReplaceProduct(originalProduct.Id, request);
-//
-//         result.Result.ShouldBeOfType<OkObjectResult>();
-//         var response = result.Result as OkObjectResult;
-//         response!.Value.ShouldBeOfType<ReplaceProductResponse>();
-//         var replaceProductResponse = response.Value as ReplaceProductResponse;
-//         replaceProductResponse.ShouldBeEquivalentTo(_extensions.ToReplaceProductResponse(originalProduct));
-//         var updatedProduct = await _dbContext.Products.FindAsync(originalProduct.Id);
-//         updatedProduct.ShouldNotBeNull();
-//         updatedProduct.ShouldBeEquivalentTo(originalProduct);
-//     }
-//
-//     public void Dispose()
-//     {
-//         _dbContext.Database.EnsureDeleted();
-//         _dbContext.Dispose();
-//         GC.SuppressFinalize(this);
-//     }
-// }
+using System.Globalization;
+using AutoFixture;
+using backend.Database;
+using backend.Product.Contracts;
+using backend.Product.Controllers;
+using backend.Product.DomainModels;
+using backend.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+
+namespace backend.Product.Tests;
+
+[Collection("SequentialExecutionCollection")]
+public class ReplaceProductControllerTests : IDisposable
+{
+    private readonly Fixture _fixture = new();
+    private readonly ReplaceProductController _controller;
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ReplaceProductExtensions _extensions;
+
+    public ReplaceProductControllerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseNpgsql("Host=localhost;Database=mytestdatabase;Username=myusername;Password=mypassword")
+            .Options;
+
+        var db = new ApplicationDbContext(options);
+        db.Database.EnsureCreated();
+        _dbContext = db;
+        _extensions = new ReplaceProductExtensions(new TypeParser());
+        _controller = new ReplaceProductController(_dbContext, _extensions);
+    }
+
+    [Fact]
+    public async Task ReplaceProduct_Should_ReturnOk_WithUpdatedProduct_When_ProductExists()
+    {
+        var originalProduct = new ProductTestDataBuilder().Build();
+        _dbContext.Products.Add(originalProduct);
+        await _dbContext.SaveChangesAsync();
+
+        var request = new ReplaceProductRequest
+        {
+            Name = _fixture.Create<string>(),
+            Price = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+            Category = _fixture.Create<Category>().ToString(),
+            Dimensions = new DimensionsContract
+            {
+                Length = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                Width = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture),
+                Height = _fixture.Create<decimal>().ToString(CultureInfo.InvariantCulture)
+            }
+        };
+
+        var result = await _controller.ReplaceProduct(originalProduct.Id, request);
+
+        result.Result.ShouldBeOfType<OkObjectResult>();
+        var response = result.Result as OkObjectResult;
+        response!.Value.ShouldBeOfType<ReplaceProductResponse>();
+        var replaceProductResponse = response.Value as ReplaceProductResponse;
+        replaceProductResponse.ShouldBeEquivalentTo(_extensions.ToReplaceProductResponse(originalProduct));
+        var updatedProduct = await _dbContext.Products.FindAsync(originalProduct.Id);
+        updatedProduct.ShouldNotBeNull();
+        updatedProduct.ShouldBeEquivalentTo(originalProduct);
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Database.EnsureDeleted();
+        _dbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
