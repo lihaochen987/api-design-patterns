e9ddac826fb98a4df549fcad00c7c8ddcae81216
HaoChen <lihaochen987@gmail.com>
2024-12-02 06:36:35 +1300

    Move FetchClient into shared folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {$api} from "./FetchClient.ts";
+import {$api} from "./shared/FetchClient.ts";
 
 function App() {
     const [count, setCount] = useState(0)
