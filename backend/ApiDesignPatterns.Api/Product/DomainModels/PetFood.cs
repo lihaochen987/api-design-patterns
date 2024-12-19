a27dbb45acc4a7e1bb337adde53e35dcb9f088e3
HaoChen <lihaochen987@gmail.com>
2024-12-20 12:25:01 +1300

    Add data annotation to WeightKg

--- backend/ApiDesignPatterns.Api/Product/DomainModels/PetFood.cs
+++ backend/ApiDesignPatterns.Api/Product/DomainModels/PetFood.cs
@@ -5,24 +5,10 @@ namespace backend.Product.DomainModels;
 
 public class PetFood : Product
 {
-    private decimal _weightKg;
     public AgeGroup AgeGroup { get; set; }
     public BreedSize BreedSize { get; set; }
     [MaxLength(300)] public required string Ingredients { get; set; }
     public required Dictionary<string, object> NutritionalInfo { get; set; }
     [MaxLength(300)] public required string StorageInstructions { get; set; }
-
-    public decimal WeightKg
-    {
-        get => _weightKg;
-        set
-        {
-            if (value <= 0)
-            {
-                throw new ArgumentException("Weight must be greater than zero.");
-            }
-
-            _weightKg = value;
-        }
-    }
+    [Range(0.01, 1000)] public decimal WeightKg { get; set; }
 }
