f5b5fdfd9c1e7aa10d30bd485198b8745dfe24da
HaoChen <lihaochen987@gmail.com>
2025-01-19 12:22:48 +1300

    WTF

---
+++ frontend/apps/web/src/main.tsx
@@ -0,0 +1,16 @@
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import './index.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import App from "./App.tsx";
+
+const queryClient = new QueryClient();
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App/>
+        </QueryClientProvider>
+    </StrictMode>,
+)
