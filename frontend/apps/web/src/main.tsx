8c0995db356f4d728c140c73cf326eee87b43efb
HaoChen <lihaochen987@gmail.com>
2025-01-19 12:32:58 +1300

    Get frontend working

--- frontend/apps/web/src/main.tsx
+++ frontend/apps/web/src/main.tsx
@@ -1,12 +1,11 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import App from "./App.tsx";
+import React from 'react';
 
 const queryClient = new QueryClient();
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
