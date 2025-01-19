f7cb66275405af6ae95101266cb5262c16f576c0
HaoChen <lihaochen987@gmail.com>
2025-01-19 15:06:45 +1300

    Add IQueryHandler.cs

---
+++ backend/ApiDesignPatterns.Api/Shared/QueryHandler/IQueryHandler.cs
@@ -0,0 +1,13 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+namespace backend.Shared.QueryHandler;
+
+public interface IQuery<TResult>
+{
+}
+
+public interface IQueryHandler<in TQuery, out TResult> where TQuery : IQuery<TResult>
+{
+    TResult Handle(TQuery query);
+}
