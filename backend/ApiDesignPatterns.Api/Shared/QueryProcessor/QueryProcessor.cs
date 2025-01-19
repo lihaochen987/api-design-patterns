3f7a1489ff597d731a925e8ea90ef94053421cb3
HaoChen <lihaochen987@gmail.com>
2025-01-20 06:28:20 +1300

    Add stand-in QueryProcessor.cs

---
+++ backend/ApiDesignPatterns.Api/Shared/QueryProcessor/QueryProcessor.cs
@@ -0,0 +1,26 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using backend.Shared.QueryHandler;
+
+namespace backend.Shared.QueryProcessor;
+
+sealed class QueryProcessor(IServiceProvider serviceProvider) : IQueryProcessor
+{
+    [DebuggerStepThrough]
+    public Task<TResult?> Process<TResult>(IQuery<TResult> query)
+    {
+        var handlerType = typeof(IQueryHandler<,>)
+            .MakeGenericType(query.GetType(), typeof(TResult));
+
+        dynamic? handler = serviceProvider.GetService(handlerType);
+
+        if (handler == null)
+        {
+            throw new InvalidOperationException($"Handler for {handlerType.FullName} not found.");
+        }
+
+        return handler.Handle((dynamic)query);
+    }
+}
