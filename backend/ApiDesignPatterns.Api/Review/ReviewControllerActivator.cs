200382b24e628de14121a773c7e615f632c51a99
HaoChen <lihaochen987@gmail.com>
2025-02-06 10:28:59 +1300

    Add Review endpoints to ReviewControllerActivator.cs

--- backend/ApiDesignPatterns.Api/Review/ReviewControllerActivator.cs
+++ backend/ApiDesignPatterns.Api/Review/ReviewControllerActivator.cs
@@ -2,12 +2,17 @@
 // The.NET Foundation licenses this file to you under the MIT license.
 
 using AutoMapper;
+using backend.Review.ApplicationLayer.Commands.CreateReview;
+using backend.Review.ApplicationLayer.Commands.DeleteReview;
+using backend.Review.ApplicationLayer.Queries.GetReview;
 using backend.Review.ApplicationLayer.Queries.GetReviewView;
 using backend.Review.DomainModels;
+using backend.Review.InfrastructureLayer.Database.Review;
 using backend.Review.InfrastructureLayer.Database.ReviewView;
 using backend.Review.ReviewControllers;
 using backend.Review.Services;
 using backend.Shared;
+using backend.Shared.CommandHandler;
 using backend.Shared.ControllerActivators;
 using backend.Shared.FieldMask;
 using backend.Shared.QueryHandler;
@@ -43,6 +48,56 @@ public class ReviewControllerActivator : BaseControllerActivator
     {
         Type type = context.ActionDescriptor.ControllerTypeInfo.AsType();
 
+        if (type == typeof(CreateReviewController))
+        {
+            var dbConnection = CreateDbConnection();
+            TrackDisposable(context, dbConnection);
+            var repository = new ReviewRepository(dbConnection);
+
+            // CreateReview handler
+            var createReview = new CreateReviewHandler(repository);
+            var createReviewWithLogging = new LoggingCommandHandlerDecorator<CreateReviewQuery>(createReview,
+                _loggerFactory.CreateLogger<LoggingCommandHandlerDecorator<CreateReviewQuery>>());
+            var createReviewWithAudit =
+                new AuditCommandHandlerDecorator<CreateReviewQuery>(createReviewWithLogging, dbConnection);
+            var createReviewWithTransaction =
+                new TransactionCommandHandlerDecorator<CreateReviewQuery>(createReviewWithAudit, dbConnection);
+
+            return new CreateReviewController(
+                createReviewWithTransaction,
+                _mapper);
+        }
+
+        if (type == typeof(DeleteReviewController))
+        {
+            var dbConnection = CreateDbConnection();
+            TrackDisposable(context, dbConnection);
+            var repository = new ReviewRepository(dbConnection);
+
+            // GetReview handler
+            var getReview = new GetReviewHandler(repository);
+            var getReviewWithLogging = new LoggingQueryHandlerDecorator<GetReviewQuery, DomainModels.Review>(
+                getReview,
+                _loggerFactory.CreateLogger<LoggingQueryHandlerDecorator<GetReviewQuery, DomainModels.Review>>());
+            var getReviewWithValidation =
+                new ValidationQueryHandlerDecorator<GetReviewQuery, DomainModels.Review>(getReviewWithLogging);
+
+
+            // DeleteReview handler
+            var deleteReview = new DeleteReviewHandler(repository);
+            var deleteReviewWithLogging = new LoggingCommandHandlerDecorator<DeleteReviewQuery>(deleteReview,
+                _loggerFactory.CreateLogger<LoggingCommandHandlerDecorator<DeleteReviewQuery>>());
+            var deleteReviewWithAudit =
+                new AuditCommandHandlerDecorator<DeleteReviewQuery>(deleteReviewWithLogging, dbConnection);
+            var deleteReviewWithTransaction =
+                new TransactionCommandHandlerDecorator<DeleteReviewQuery>(deleteReviewWithAudit, dbConnection);
+
+            return new DeleteReviewController(
+                getReviewWithValidation,
+                deleteReviewWithTransaction);
+        }
+
+
         if (type == typeof(GetReviewController))
         {
             var dbConnection = CreateDbConnection();
@@ -54,9 +109,11 @@ public class ReviewControllerActivator : BaseControllerActivator
             var getReviewViewWithLogging = new LoggingQueryHandlerDecorator<GetReviewViewQuery, ReviewView>(
                 getReviewView,
                 _loggerFactory.CreateLogger<LoggingQueryHandlerDecorator<GetReviewViewQuery, ReviewView>>());
+            var getReviewViewWithValidation =
+                new ValidationQueryHandlerDecorator<GetReviewViewQuery, ReviewView>(getReviewViewWithLogging);
 
             return new GetReviewController(
-                getReviewViewWithLogging,
+                getReviewViewWithValidation,
                 _fieldMaskConverterFactory,
                 _mapper);
         }
