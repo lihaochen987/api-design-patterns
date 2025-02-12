75b7c61aa35b2a75d1ac37c0eb261a427ee0ebb2
HaoChen <lihaochen987@gmail.com>
2025-02-13 06:32:26 +1300

    Add GetSupplierHandlerTests.cs

---
+++ backend/ApiDesignPatterns.Api/Supplier/Tests/ApplicationLayerTests/GetSupplierHandlerTests.cs
@@ -0,0 +1,39 @@
+// Licensed to the.NET Foundation under one or more agreements.
+// The.NET Foundation licenses this file to you under the MIT license.
+
+using backend.Shared.QueryHandler;
+using backend.Supplier.ApplicationLayer.Queries.GetSupplier;
+using backend.Supplier.Tests.TestHelpers.Builders;
+using Shouldly;
+using Xunit;
+
+namespace backend.Supplier.Tests.ApplicationLayerTests;
+
+public class GetSupplierHandlerTests : GetSupplierHandlerTestBase
+{
+    [Fact]
+    public async Task Handle_ReturnsSupplier_WhenSupplierExists()
+    {
+        DomainModels.Supplier expectedSupplier = new SupplierTestDataBuilder().Build();
+        Repository.Add(expectedSupplier);
+        IQueryHandler<GetSupplierQuery, DomainModels.Supplier> sut = GetSupplierHandler();
+
+        DomainModels.Supplier? result = await sut.Handle(new GetSupplierQuery { Id = expectedSupplier.Id });
+
+        result.ShouldNotBeNull();
+        result.ShouldBeEquivalentTo(expectedSupplier);
+        Repository.CallCount.ShouldContainKeyAndValue("GetSupplierAsync", 1);
+    }
+
+    [Fact]
+    public async Task Handle_ReturnsNull_WhenSupplierDoesNotExist()
+    {
+        DomainModels.Supplier nonExistentSupplier = new SupplierTestDataBuilder().Build();
+        IQueryHandler<GetSupplierQuery, DomainModels.Supplier> sut = GetSupplierHandler();
+
+        DomainModels.Supplier? result = await sut.Handle(new GetSupplierQuery { Id = nonExistentSupplier.Id });
+
+        result.ShouldBeNull();
+        Repository.CallCount.ShouldContainKeyAndValue("GetSupplierAsync", 1);
+    }
+}
