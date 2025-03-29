fa1f408b73ae88925e795ee9e8b2d2e205f0e1b0
HaoChen <lihaochen987@gmail.com>
2025-03-29 13:24:32 +1300

    Remove dead code

--- backend/ApiDesignPatterns.Api/Program.cs
+++ backend/ApiDesignPatterns.Api/Program.cs
@@ -2,14 +2,12 @@ using System.Data;
 using System.Text.Json.Serialization;
 using backend.Inventory;
 using backend.Product;
-using backend.Product.ProductControllers;
 using backend.Review;
 using backend.Shared;
 using backend.Shared.ControllerActivators;
 using backend.Supplier;
 using DbUp;
 using DbUp.Engine;
-using MassTransit;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Npgsql;
 
@@ -111,29 +109,6 @@ app.MapControllers();
 
 app.Run();
 return;
-
-// static void ConfigureMessageQueue<TRequest, TResponse>(IBusRegistrationConfigurator x)
-//     where TRequest : class
-//     where TResponse : class
-// {
-//     x.UsingRabbitMq((_, cfg) =>
-//     {
-//         cfg.Host("rabbitmq", "/", h =>
-//         {
-//             h.Username("guest");
-//             h.Password("guest");
-//         });
-//         cfg.UseMessageRetry(r =>
-//         {
-//             r.Intervals(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10));
-//             r.Handle<RabbitMQ.Client.Exceptions.BrokerUnreachableException>();
-//         });
-//         cfg.UseScheduledRedelivery(r => r.Intervals(TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(15)));
-//
-//         cfg.Message<TRequest>(e => e.SetEntityName($"{typeof(TRequest).Name.ToLower()}-queue"));
-//         cfg.Message<TResponse>(e => e.SetEntityName($"{typeof(TResponse).Name.ToLower()}-queue"));
-//     });
-// }
 
 void ApplyMigrations(
     string connectionString,
