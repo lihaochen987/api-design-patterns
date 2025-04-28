b1374576f6a8ce88b1de4a5cfc97e0d8d852c623
HaoChen Li <lihaochen987@gmail.com>
2025-04-29 08:27:45 +1200

    Move TypeHandlers to their value objects

--- backend/ApiDesignPatterns.Api/Product/DomainModels/ValueObjects/IngredientsTypeHandler.cs
+++ backend/ApiDesignPatterns.Api/Product/DomainModels/ValueObjects/IngredientsTypeHandler.cs
@@ -1,10 +1,9 @@
 ﻿// Licensed to the.NET Foundation under one or more agreements.
 // The.NET Foundation licenses this file to you under the MIT license.
 
-using backend.Product.DomainModels.ValueObjects;
 using backend.Shared.SqlMappers;
 
-namespace backend.Product.InfrastructureLayer.Mappers;
+namespace backend.Product.DomainModels.ValueObjects;
 
 public class IngredientsTypeHandler : StringValueObjectTypeHandler<Ingredients>
 {
