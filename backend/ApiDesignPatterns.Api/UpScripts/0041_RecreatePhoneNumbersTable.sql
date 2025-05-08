c857fef4cd719046f7d94a114d0b069a872ecd4c
HaoChen Li <lihaochen987@gmail.com>
2025-05-08 15:06:15 +1200

    Restructure tables to have 1-N with PhoneNumbers

---
+++ backend/ApiDesignPatterns.Api/UpScripts/0041_RecreatePhoneNumbersTable.sql
@@ -0,0 +1,18 @@
+DO
+$$
+    BEGIN
+        DROP TABLE IF EXISTS supplier_phone_numbers CASCADE;
+
+        CREATE TABLE phone_numbers
+        (
+            phone_number_id           SERIAL PRIMARY KEY,
+            supplier_id               BIGINT
+                CONSTRAINT fk_supplier_id
+                    REFERENCES suppliers (supplier_id)
+                    ON DELETE CASCADE,
+            phone_number_country_code VARCHAR(10),
+            phone_number_area_code    VARCHAR(10),
+            phone_number_digits       VARCHAR(50)
+        );
+    END
+$$;
---
+++ backend/ApiDesignPatterns.Api/UpScripts/0043_RecreateSupplierView.sql
@@ -0,0 +1,19 @@
+DO
+$$
+    BEGIN
+        CREATE OR REPLACE VIEW suppliers_view AS
+        SELECT s.supplier_id,
+               CONCAT(s.supplier_firstname, ' ', s.supplier_lastname) AS supplier_fullname,
+               s.supplier_email,
+               s.supplier_created_at,
+               sa.supplier_address_street,
+               sa.supplier_address_city,
+               sa.supplier_address_postal_code,
+               sa.supplier_address_country,
+               CONCAT(spn.phone_number_country_code, ' ', spn.phone_number_area_code, ' ',
+                      spn.phone_number_digits)                        AS supplier_phone_number
+        FROM suppliers s
+                 LEFT JOIN phone_numbers spn ON spn.supplier_id = s.supplier_id
+                 LEFT JOIN supplier_addresses sa ON sa.supplier_id = s.supplier_id;
+    END
+$$;
