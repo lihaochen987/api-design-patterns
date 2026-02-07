456cacdd0bb95ba48cf52c3769cbc0d23c852e37
HaoChen Li <lihaochen987@gmail.com>
2026-02-08 08:21:23 +1300

    Add public id to review_view

---
+++ backend/ApiDesignPatterns.Api/UpScripts/0054_AddPublicIdColumnToReviewTable.sql
@@ -0,0 +1,16 @@
+﻿DO
+$$
+    BEGIN
+        ALTER TABLE reviews
+            ADD COLUMN review_public_id UUID DEFAULT gen_random_uuid();
+
+        UPDATE reviews
+        SET review_public_id = gen_random_uuid()
+        WHERE review_public_id IS NULL;
+
+        ALTER TABLE reviews
+            ALTER COLUMN review_public_id SET NOT NULL;
+
+        CREATE UNIQUE INDEX IX_reviews_public_id ON reviews (review_public_id);
+    END;
+$$
